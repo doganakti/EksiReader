e3aa3a7a9a73c27538a59460e86d2c0f1e4ff5a9
doganakti <doganakti@gmail.com>
2019-05-01 17:09:10 +0300

    Resolved dependency conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:eksi_reader/app.dart';
-import 'package:eksi_reader/main.reflectable.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  initializeReflectable();
   runApp(App());
 }
